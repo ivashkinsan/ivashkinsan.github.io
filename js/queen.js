7554e6212c8ea5482581eb7b871a3e9cabdcf883
ivashkinsan <64753179+ivashkinsan@users.noreply.github.com>
2021-10-31 10:06:15 +0300

    Добавил звуки piano

---
+++ js/queen.js
@@ -0,0 +1,28 @@
+let keys = document.querySelectorAll(".key");
+// console.log(allKeys);
+
+
+for (let i = 0; i < keys.length; i++){
+    keys[i].addEventListener('click', function () {
+        console.log('KLICK');
+        let data = keys[i].getAttribute('data-key');
+        console.log(data);
+        playNote(data);
+          });
+          
+    
+};
+
+function playNote(e) {
+    const audioAll = document.querySelectorAll('audio');
+    audio = audioAll[e - 1];
+    console.log(audio);
+    // if (!key) return;
+    // const keyNote = key.getAttribute("data-note");
+    // key.classList.add("playing");
+    // note.innerHTML = keyNote;
+    audio.currentTime = 0;
+    audio.play();
+  }
+  
+ 
