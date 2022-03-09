897c2a3114877534514e3cfee9c17207a78522fc
ivashkinsan <64753179+ivashkinsan@users.noreply.github.com>
2022-03-09 17:36:23 +0300

    new i

--- js/symmetry_workout.js
+++ js/symmetry_workout.js
@@ -1,5 +1,50 @@
 document.oncontextmenu = cmenu; function cmenu() { return false; };
 
+let infoObj = {
+    'symmetry2': ['симметрия 2х',0,0],
+    'symmetry3': ['симметрия 3х',0,0],
+    'symmetry4': ['симметрия 4х',0,0],
+    'symmetry6': ['симметрия 6ти',0,0],
+    'maj_tetrachord': ['тетрахорд маж',0,0],
+    'min_tetrachord': ['тетрахорд мин',0,0],
+    'frig_tetrachord': ['тетрахорд фриг',0,0],
+    'harm_tetrachord': ['тетрахорд гарм',0,0],
+    'whole_tetrachord': ['тетрахорд ТТТ',0,0],
+    'm2': ['м2',0,0],
+    'B2': ['Б2',0,0],
+    'm3': ['м3',0,0],
+    'B3': ['Б3',0,0],
+    'ch4': ['ч4',0,0],
+    'TTT': ['ТТТ',0,0],
+    'ch5': ['ч5',0,0],
+    'm6': ['м6',0,0],
+    'B6': ['Б6',0,0],
+    'm7': ['м7',0,0],
+    'B7': ['Б7',0,0],
+    'maj': ['маж',0,0],
+    'min': ['мин',0,0],
+    'frig': ['фриг',0,0],
+    'harm': ['гарм',0,0],
+    'TTT': ['ТТТ',0,0],
+    'TTT_&_maj': ['лидийский',0,0],
+    'maj_&_maj': ['ионийский',0,0],
+    'maj_&_min': ['миксолидийский',0,0],
+    'min_&_min': ['дорийский',0,0],
+    'min_&_frig': ['эолийский',0,0],
+    'frig_&_frig': ['фригийский',0,0],
+    'frig_&_TTT': ['локрийский',0,0],
+    'aug_triads': ['трезвучие увеличенное',0,0],
+    'maj_triads': ['трезвучие мажорное',0,0],
+    'min_triads': ['трезвучие минорное',0,0],
+    'dim_triads': ['трезвучие уменьшённое',0,0],
+    'sus2_triads': ['sus2',0,0],
+    'sus4_triads': ['sus4',0,0],
+    'maj_add2_triads': ['maj add2',0,0],
+    'min_add2_triads': ['min add2',0,0],
+    'maj_add4_triads': ['maj add4',0,0],
+    'min_add4_triads': ['min add4',0,0]
+};
+
 let marker = document.querySelectorAll('p');
 for(let item of marker){
     item.innerHTML = '';
@@ -114,37 +159,39 @@ function getRandomIntInclusive(min, max) {
     if(symBtnLevel.dataset.number == 'harm_tetrachord'){symBtnLevelGO = [(ti+1),(ti+2),(ti+5),(ti+6)];}
     if(symBtnLevel.dataset.number == 'whole_tetrachord'){symBtnLevelGO = [(ti+1),(ti+3),(ti+5),(ti+7)];}
 
-    if(symBtnLevel.dataset.number == '3_AUG'){symBtnLevelGO = [(ti+1),(ti+5),(ti+9)];}
-    if(symBtnLevel.dataset.number == '3_MAJ'){symBtnLevelGO = [(ti+1),(ti+5),(ti+8)];}
-    if(symBtnLevel.dataset.number == '3_MIN'){symBtnLevelGO = [(ti+1),(ti+4),(ti+8)];}
-    if(symBtnLevel.dataset.number == '3_DIM'){symBtnLevelGO = [(ti+1),(ti+4),(ti+7)];}
-    if(symBtnLevel.dataset.number == '3_SUS2'){symBtnLevelGO = [(ti+1),(ti+3),(ti+8)];}
-    if(symBtnLevel.dataset.number == '3_SUS4'){symBtnLevelGO = [(ti+1),(ti+6),(ti+8)];}
-    if(symBtnLevel.dataset.number == '3_ADD2_maj'){symBtnLevelGO = [(ti+1),(ti+3),(ti+5),(ti+8)];}
-    if(symBtnLevel.dataset.number == '3_ADD2_min'){symBtnLevelGO = [(ti+1),(ti+3),(ti+4),(ti+8)];}
-    if(symBtnLevel.dataset.number == '3_ADD4_maj'){symBtnLevelGO = [(ti+1),(ti+5),(ti+6),(ti+8)];}
-    if(symBtnLevel.dataset.number == '3_ADD4_min'){symBtnLevelGO = [(ti+1),(ti+4),(ti+6),(ti+8)];}
+    if(symBtnLevel.dataset.number == 'aug_triads'){symBtnLevelGO = [(ti+1),(ti+5),(ti+9)];}
+    if(symBtnLevel.dataset.number == 'maj_triads'){symBtnLevelGO = [(ti+1),(ti+5),(ti+8)];}
+    if(symBtnLevel.dataset.number == 'min_triads'){symBtnLevelGO = [(ti+1),(ti+4),(ti+8)];}
+    if(symBtnLevel.dataset.number == 'dim_triads'){symBtnLevelGO = [(ti+1),(ti+4),(ti+7)];}
+    if(symBtnLevel.dataset.number == 'sus2_triads'){symBtnLevelGO = [(ti+1),(ti+3),(ti+8)];}
+    if(symBtnLevel.dataset.number == 'sus4_triads'){symBtnLevelGO = [(ti+1),(ti+6),(ti+8)];}
+    if(symBtnLevel.dataset.number == 'maj_add2_triads'){symBtnLevelGO = [(ti+1),(ti+3),(ti+5),(ti+8)];}
+    if(symBtnLevel.dataset.number == 'min_add2_triads'){symBtnLevelGO = [(ti+1),(ti+3),(ti+4),(ti+8)];}
+    if(symBtnLevel.dataset.number == 'maj_add4_triads'){symBtnLevelGO = [(ti+1),(ti+5),(ti+6),(ti+8)];}
+    if(symBtnLevel.dataset.number == 'min_add4_triads'){symBtnLevelGO = [(ti+1),(ti+4),(ti+6),(ti+8)];}
 
-    if(symBtnLevel.dataset.number == 'М2'){symBtnLevelGO = [(ti+1),(ti+2)];}
-    if(symBtnLevel.dataset.number == 'Б2'){symBtnLevelGO = [(ti+1),(ti+3)];}
-    if(symBtnLevel.dataset.number == 'М3'){symBtnLevelGO = [(ti+1),(ti+4)];}
-    if(symBtnLevel.dataset.number == 'Б3'){symBtnLevelGO = [(ti+1),(ti+5)];}
-    if(symBtnLevel.dataset.number == 'ч4'){symBtnLevelGO = [(ti+1),(ti+6)];}
-    if(symBtnLevel.dataset.number == 'ТТТ'){symBtnLevelGO = [(ti+1),(ti+7)];}
-    if(symBtnLevel.dataset.number == 'ч5'){symBtnLevelGO = [(ti+1),(ti+8)];}
-    if(symBtnLevel.dataset.number == 'М6'){symBtnLevelGO = [(ti+1),(ti+9)];}
-    if(symBtnLevel.dataset.number == 'Б6'){symBtnLevelGO = [(ti+1),(ti+10)];}
-    if(symBtnLevel.dataset.number == 'М7'){symBtnLevelGO = [(ti+1),(ti+11)];}
-    if(symBtnLevel.dataset.number == 'Б7'){symBtnLevelGO = [(ti+1),(ti+12)];}
+    if(symBtnLevel.dataset.number == 'm2'){symBtnLevelGO = [(ti+1),(ti+2)];}
+    if(symBtnLevel.dataset.number == 'B2'){symBtnLevelGO = [(ti+1),(ti+3)];}
+    if(symBtnLevel.dataset.number == 'm3'){symBtnLevelGO = [(ti+1),(ti+4)];}
+    if(symBtnLevel.dataset.number == 'B3'){symBtnLevelGO = [(ti+1),(ti+5)];}
+    if(symBtnLevel.dataset.number == 'ch4'){symBtnLevelGO = [(ti+1),(ti+6)];}
+    if(symBtnLevel.dataset.number == 'TTT'){symBtnLevelGO = [(ti+1),(ti+7)];}
+    if(symBtnLevel.dataset.number == 'ch5'){symBtnLevelGO = [(ti+1),(ti+8)];}
+    if(symBtnLevel.dataset.number == 'm6'){symBtnLevelGO = [(ti+1),(ti+9)];}
+    if(symBtnLevel.dataset.number == 'B6'){symBtnLevelGO = [(ti+1),(ti+10)];}
+    if(symBtnLevel.dataset.number == 'm7'){symBtnLevelGO = [(ti+1),(ti+11)];}
+    if(symBtnLevel.dataset.number == 'B7'){symBtnLevelGO = [(ti+1),(ti+12)];}
 
-    if(symBtnLevel.dataset.number == 'MAJ_DIATONIK_LYD'){symBtnLevelGO = [(ti+1),(ti+3),(ti+5),(ti+7),(ti+8),(ti+10),(ti+12),(ti+13)];}
-    if(symBtnLevel.dataset.number == 'MAJ_DIATONIK_ION'){symBtnLevelGO = [(ti+1),(ti+3),(ti+5),(ti+6),(ti+8),(ti+10),(ti+12),(ti+13)];}
-    if(symBtnLevel.dataset.number == 'MAJ_DIATONIK_MIX'){symBtnLevelGO = [(ti+1),(ti+3),(ti+5),(ti+6),(ti+8),(ti+10),(ti+11),(ti+13)];}
-    if(symBtnLevel.dataset.number == 'MAJ_DIATONIK_DOR'){symBtnLevelGO = [(ti+1),(ti+3),(ti+4),(ti+6),(ti+8),(ti+10),(ti+11),(ti+13)];}
-    if(symBtnLevel.dataset.number == 'MAJ_DIATONIK_EOL'){symBtnLevelGO = [(ti+1),(ti+3),(ti+4),(ti+6),(ti+8),(ti+9),(ti+11),(ti+13)];}
-    if(symBtnLevel.dataset.number == 'MAJ_DIATONIK_FRI'){symBtnLevelGO = [(ti+1),(ti+2),(ti+4),(ti+6),(ti+8),(ti+9),(ti+11),(ti+13)];}
-    if(symBtnLevel.dataset.number == 'MAJ_DIATONIK_LOC'){symBtnLevelGO = [(ti+1),(ti+2),(ti+4),(ti+6),(ti+7),(ti+9),(ti+11),(ti+13)];}
-};
+    if(symBtnLevel.dataset.number == 'TTT_&_maj'){symBtnLevelGO = [(ti+1),(ti+3),(ti+5),(ti+7),(ti+8),(ti+10),(ti+12),(ti+13)];}
+    if(symBtnLevel.dataset.number == 'maj_&_maj'){symBtnLevelGO = [(ti+1),(ti+3),(ti+5),(ti+6),(ti+8),(ti+10),(ti+12),(ti+13)];}
+    if(symBtnLevel.dataset.number == 'maj_&_min'){symBtnLevelGO = [(ti+1),(ti+3),(ti+5),(ti+6),(ti+8),(ti+10),(ti+11),(ti+13)];}
+    if(symBtnLevel.dataset.number == 'min_&_min'){symBtnLevelGO = [(ti+1),(ti+3),(ti+4),(ti+6),(ti+8),(ti+10),(ti+11),(ti+13)];}
+    if(symBtnLevel.dataset.number == 'min_&_frig'){symBtnLevelGO = [(ti+1),(ti+3),(ti+4),(ti+6),(ti+8),(ti+9),(ti+11),(ti+13)];}
+    if(symBtnLevel.dataset.number == 'frig_&_frig'){symBtnLevelGO = [(ti+1),(ti+2),(ti+4),(ti+6),(ti+8),(ti+9),(ti+11),(ti+13)];}
+    if(symBtnLevel.dataset.number == 'frig_&_TTT'){symBtnLevelGO = [(ti+1),(ti+2),(ti+4),(ti+6),(ti+7),(ti+9),(ti+11),(ti+13)];}
+
+    
+  };
 
 //ПОИСК ВЫБРАННЫХ ЭЛЕМЕНТОВ
 let finderLed = function () {
@@ -169,9 +216,16 @@ let sravniElem = function(a,b){
     if(a.length == b.length && a.length == sovpadeniya){
         // console.log('rabotaet' + sovpadeniya);
         // console.log(answerArr);
+        infoObj[symBtnLevel.dataset.number][1] += 1;
         win_ledOn(answerArr);
         setTimeout(startWork,1000);
-    }
+    } 
+    // else {infoObj[symBtnLevel.dataset.number][2] += 0.5;}
+    
+    // console.log(symBtnLevel.dataset.number);
+    // console.log(infoObj.symmetry2);
+    // console.log(infoObj)
+
 };
 
 //функция подсветки в случае верного ответа   
@@ -179,17 +233,26 @@ let win_ledOn = function(winArr){
     for (let item of winArr) {
         item.classList.add('ledWin');  
     };
+    create_result();
 };
 
-// let monitoring = function(level){
-//    if(level == 'symmetry2'){monitor.textContent = 'СИММЕТРИЯ из 2';}
-//    if(level == 'symmetry3'){monitor.textContent = 'СИММЕТРИЯ из 3';}
-//    if(level == 'symmetry4'){monitor.textContent = 'СИММЕТРИЯ из 4';}
-//    if(level == 'symmetry6'){monitor.textContent = 'СИММЕТРИЯ из 6';}
-// };
+
+// СОЗДАНИЕ СПИСКА РЕЗУЛЬТАТОВ
+let create_result = function(){
+      let result_ul = document.querySelector('.result_ul');
+      let arr_info_obj = Object.keys(infoObj);
+      // console.log(result_ul.firstChild);
+      for(i = 0; i < Object.keys(infoObj).length && result_ul.firstChild; i++){
+      result_ul.firstChild.remove();
+      }
+      for(i = 0; i < Object.keys(infoObj).length; i++){
+    let result_ul_li = document.createElement('li');
+    result_ul_li.innerHTML = infoObj[arr_info_obj[i]][0] + ' - ' + infoObj[arr_info_obj[i]][1];
+    result_ul.append(result_ul_li);
+  }
+};
 
 //SLIDER ZONE
-
 const slider = document.querySelector(".slider");
 
 const slides = slider.querySelectorAll(".slide");
@@ -239,3 +302,6 @@ const init = function () {
 };
 
 init();
+
+let popup_text = document.querySelector('.popup-text');
+// console.log(popup_text);
