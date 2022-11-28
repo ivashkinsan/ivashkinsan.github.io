bf7e21751a4f4681ed4fb302523790e6c37efda0
ivashkinsan <64753179+ivashkinsan@users.noreply.github.com>
2022-11-28 21:23:57 +0300

    new rythm

--- js/rythm.js
+++ js/rythm.js
@@ -1,3 +1,6 @@
+document.oncontextmenu = function () {
+    return false
+};
 const click_items = document.querySelectorAll('.click_item');
 const p_numb = document.querySelectorAll('.numb');
 
@@ -153,123 +156,27 @@ function monitoringStart(all) {
     console.log(monitoringBaseArr);
 }
 
+// изменение формы круга в квадрат
 document.querySelector('.button_square').onclick = function () {
     for (let item of click_items) {
         item.classList.toggle('click_item_square');
     }
 }
 
-
+// изменение центровки длительностей
 document.querySelector('.button_position').onclick = function () {
     for (let item of click_items) {
         item.classList.toggle('click_item_position');
     }
 }
 
+// кнопка показа длительностей
 document.querySelector('.button_label_duration').onclick = function () {
     addFontNote();
     addFontSize();
 }
 
-document.querySelector('.button_label_numb').onclick = function () {
-    addFontSize();
-    for (let item of p_numb) {
-        switch (item.dataset.note) {
-            case '1':
-                item.innerHTML = '1';
-                break;
-            case '2':
-                item.innerHTML = '2';
-                break;
-            case '4':
-                item.innerHTML = '4';
-                break;
-            case '8':
-                item.innerHTML = '8';
-                break;
-            case '16':
-                item.innerHTML = '16';
-                break;
-        }
-
-    }
-}
-
-function speedCalculation(e) {
-    for (let item of p_numb) {
-        switch (item.dataset.note) {
-            case '1':
-                item.innerHTML = e / 2;
-                break;
-            case '2':
-                item.innerHTML = e / 4;
-                break;
-            case '4':
-                item.innerHTML = e / 8;
-                break;
-            case '8':
-                item.innerHTML = e / 16;
-                break;
-            case '16':
-                item.innerHTML = e / 32;
-                break;
-        }
-    }
-}
-
-
-
-
-
-document.querySelector('.button_label_16').onclick = () => {
-    speedCalculation(32);
-}
-
-document.querySelector('.button_label_8').onclick = () => {
-    speedCalculation(16);
-}
-
-document.querySelector('.button_label_16').addEventListener('dblclick', (event) => {
-    speedCalculationProporzion(16);
-    // ondblclick = (event) => {};
-
-})
-
-function speedCalculationProporzion(e) {
-    for (let item of p_numb) {
-        switch (item.dataset.note) {
-            case '1':
-                item.innerHTML = e * 1 + '/' + e;
-                break;
-            case '2':
-                item.innerHTML = e / 2 + '/' + e;
-                break;
-            case '4':
-                item.innerHTML = e / 4 + '/' + e;
-                break;
-            case '8':
-                item.innerHTML = e / 8 + '/' + e;
-                break;
-            case '16':
-                item.innerHTML = e / 16 + '/' + e;
-                break;
-        }
-    }
-}
-
-
-document.querySelector('.button_label_4').onclick = () => {
-    speedCalculation(8);
-}
-
-document.querySelector('.button_label_2').onclick = () => {
-    speedCalculation(4);
-}
-
-document.querySelector('.button_label_1').onclick = () => {
-    speedCalculation(2);
-}
-
+// кнопка паузы
 document.querySelector('.button_label_pause').onclick = addPause;
 
 function addPause() {
@@ -295,6 +202,120 @@ function addPause() {
     }
 }
 
+// кнопка показа чисел
+document.querySelector('.button_label_numb').onclick = function () {
+    addFontSize();
+    for (let item of p_numb) {
+        switch (item.dataset.note) {
+            case '1':
+                item.innerHTML = '1';
+                break;
+            case '2':
+                item.innerHTML = '2';
+                break;
+            case '4':
+                item.innerHTML = '4';
+                break;
+            case '8':
+                item.innerHTML = '8';
+                break;
+            case '16':
+                item.innerHTML = '16';
+                break;
+        }
+
+    }
+}
+
+// функция расчета скорости 1 2 4 8 16
+function speedCalculation(e) {
+    for (let item of p_numb) {
+        switch (item.dataset.note) {
+            case '1':
+                item.innerHTML = e / 2;
+                break;
+            case '2':
+                item.innerHTML = e / 4;
+                break;
+            case '4':
+                item.innerHTML = e / 8;
+                break;
+            case '8':
+                item.innerHTML = e / 16;
+                break;
+            case '16':
+                item.innerHTML = e / 32;
+                break;
+        }
+    }
+}
+
+
+
+
+// кнопка скорости шестнадцатых
+document.querySelector('.button_label_16').onclick = () => {
+    speedCalculation(32);
+}
+
+// кнопка скорости восьмых
+document.querySelector('.button_label_8').onclick = () => {
+    speedCalculation(16);
+}
+// кнопка скорости четвертей
+document.querySelector('.button_label_4').onclick = () => {
+    speedCalculation(8);
+}
+
+// кнопка скорости половинных
+document.querySelector('.button_label_2').onclick = () => {
+    speedCalculation(4);
+}
+
+// кнопка скорости целых
+document.querySelector('.button_label_1').onclick = () => {
+    speedCalculation(2);
+}
+
+// двойная кнопка скорости шестнадцатых
+document.querySelector('.button_label_16').addEventListener('dblclick', (event) => {
+    speedCalculationProporzion(16);
+})
+
+// двойная кнопка скорости восьмых
+document.querySelector('.button_label_8').addEventListener('dblclick', (event) => {
+    speedCalculationProporzion(8);
+})
+
+// двойная кнопка скорости четвертей
+document.querySelector('.button_label_4').addEventListener('dblclick', (event) => {
+    speedCalculationProporzion(4);
+})
+
+// двойного клика на кнопке
+function speedCalculationProporzion(e) {
+    for (let item of p_numb) {
+        switch (item.dataset.note) {
+            case '1':
+                item.innerHTML = e * 1 + '/' + e;
+                break;
+            case '2':
+                item.innerHTML = e / 2 + '/' + e;
+                break;
+            case '4':
+                item.innerHTML = e / 4 + '/' + e;
+                break;
+            case '8':
+                item.innerHTML = e / 8 + '/' + e;
+                break;
+            case '16':
+                item.innerHTML = e / 16 + '/' + e;
+                break;
+        }
+    }
+}
+
+// кнопка проценты
 document.querySelector('.button_procent').onclick = () => {
 
     for (let item of p_numb) {
@@ -318,12 +339,13 @@ document.querySelector('.button_procent').onclick = () => {
     }
 }
 
-
+// функция подключения простого шрифта
 function addBodyFontOPUSTEXT() {
     addFontSize();
     document.querySelector('body').style.fontFamily = 'OPUSTEXT';
 }
 
+// кнопка сброса
 document.querySelector('.button_reset_shadow').onclick = () => {
     addFontNote();
     addFontSize();
@@ -331,5 +353,30 @@ document.querySelector('.button_reset_shadow').onclick = () => {
         // console.log('reset');
         item.classList.remove('border_active');
         item.classList.remove('border_active_doble');
+    }
+}
+
+// действие правой кнопкой мыши
+for (let item of click_items) {
+    item.addEventListener('contextmenu', function add(elem) {
+        console.log(item);
+        elem.preventDefault();
+        elem.stopPropagation(); // глушим всплывающее событие
+        this.children;
+        item.children[0].classList.toggle('hide');
+    })
+}
+
+// кнопка прозрачности
+document.querySelector('.button_opacity_on').onclick = () => {
+    for (let item of click_items) {
+        item.children[0].classList.add('hide');
+    }
+}
+
+// кнопка прозрачности
+document.querySelector('.button_opacity_off').onclick = () => {
+    for (let item of p_numb) {
+        item.classList.remove('hide');
     }
 }
