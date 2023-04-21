b557336abf8ff8d416c62d2d20f9cf5a032d9729
ivashkinsan <64753179+ivashkinsan@users.noreply.github.com>
2023-04-21 13:40:53 +0300

    new 6

--- js/tonality_workout_copy.js
+++ js/tonality_workout_copy.js
@@ -1,13 +1,31 @@
 let keyboard_elements = document.querySelectorAll('.elem_of_keyboard');
-let white_key_numb = ['1','3','5','6','8','10','12','13','15','17','18','20','22','24','25'];
+let white_key_numb = ['1', '3', '5', '6', '8', '10', '12', '13', '15', '17', '18', '20', '22', '24', '25'];
+let all_table_rows = document.querySelectorAll('.table_rows');
 
-for(let item of keyboard_elements){
-    console.log(item.dataset.numb == ('1' || '3' || '5'||'6'||'8'||'10'||'12'))
-    if(white_key_numb.includes(item.dataset.numb)){
-        item.classList.add('white_elem')
-    } else {
-        item.classList.add('black_elem')
+// стартовое распределение цвета клавиш большой клваиатуры
+let big_keyboard_add_color = function () {
+    for (let item of keyboard_elements) {
+        console.log(item.dataset.numb == ('1' || '3' || '5' || '6' || '8' || '10' || '12'))
+        if (white_key_numb.includes(item.dataset.numb)) {
+            item.classList.add('white_elem')
+        } else {
+            item.classList.add('black_elem')
+        }
     }
-}
-    
 
+}
+
+big_keyboard_add_color();
+
+for (let item of all_table_rows) {
+
+    console.log(item.dataset.sortnumb);
+}
+console.log(all_table_rows);
+console.log('-------------------------------');
+
+all_table_rows.forEach(function (currentValue, currentIndex, listObj) {
+    console.log(currentValue);
+    console.log(currentIndex);
+    console.log(listObj);
+});
