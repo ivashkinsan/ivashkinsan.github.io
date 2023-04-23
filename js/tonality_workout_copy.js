a4dceab31ded91d8e38a4832a3873c1e989ee8f2
ivashkinsan <64753179+ivashkinsan@users.noreply.github.com>
2023-04-23 10:15:49 +0300

    new 7

--- js/tonality_workout_copy.js
+++ js/tonality_workout_copy.js
@@ -29,3 +29,45 @@ all_table_rows.forEach(function (currentValue, currentIndex, listObj) {
     console.log(currentIndex);
     console.log(listObj);
 });
+
+let main_table_grid = document.querySelector('.main__table_grid');
+let current_active_box;
+
+for (let item of all_table_rows) {
+    item.addEventListener('mouseenter', (event) => {
+        console.log(item.dataset.ton);
+        item.classList.add('active_box_border');
+    })
+}
+
+for (let item of all_table_rows) {
+    item.addEventListener('mouseleave', (event) => {
+        console.log(item);
+        if (item.classList.contains('active_box_border')) {
+            item.classList.remove('active_box_border');
+        }
+
+    })
+}
+
+// main_table_grid.addEventListener('mouseover', (event) => {
+
+//     // console.log(event);
+//     // console.log(event.target.classList.contains('table_rows'));
+//     if (current_active_box) {
+//         current_active_box.classList.remove('active_box_border');
+//     }
+//     if (event.target.classList.contains('table_rows')) {
+//         event.target.classList.add('active_box_border');
+//         event.stopPropagation();
+//         current_active_box = event.target;
+//     }
+// })
+
+// main_table_grid.addEventListener('mouseout', (event) => {
+//     console.log(event);
+//     console.log(event.target.classList.contains('table_rows'));
+//     if (event.target.classList.contains('active_box_border')) {
+//         event.target.classList.remove('active_box_border');
+//     }
+// })
