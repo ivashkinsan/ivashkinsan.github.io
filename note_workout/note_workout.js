21ee08c2a1f50a365502d43ed5c2f956c60a52c7
ivashkinsan <64753179+ivashkinsan@users.noreply.github.com>
2023-04-30 23:26:12 +0300

    new idea

--- note_workout/note_workout.js
+++ note_workout/note_workout.js
@@ -1,4 +1,5 @@
 let all_line = document.querySelectorAll('.line');
+let all_keyb_elem = document.querySelectorAll('.keyb_elem');
 
 let new_note_func = function () {
     let new_elem = document.createElement('div');
@@ -6,70 +7,98 @@ let new_note_func = function () {
     return new_elem;
 }
 
-let arr_matrix_column = {
-    'C1': 0,
-    'D1': 8,
-    'E1': 16,
-    'F1': 0,
-    'G1': 8,
-    'A1': 16,
-    'B1': 24,
 
-    'C2': 0,
-    'D2': 8,
-    'E2': 16,
-    'F2': 0,
-    'G2': 8,
-    'A2': 16,
-    'B2': 24,
-
-    'C3': 0,
-    'D3': 8,
-    'E3': 16,
-    'F3': 0,
-    'G3': 8,
-    'A3': 16,
-    'B3': 24,
-
-    'C4': 0,
-    'D4': 8,
-    'E4': 16,
-    'F4': 0,
-    'G4': 8,
-    'A4': 16,
-    'B4': 24,
-
-    'C5': 0
-};
-
-// let left_line_start = 38;
-// for (let item of all_line) {
-//     let new_el = new_note_func();
-//     if (arr_matrix_column[item.dataset.note] >= 0) {
-//         new_el.style.left = left_line_start + arr_matrix_column[item.dataset.note] + 'vh';
-//         console.log(left_line_start + arr_matrix_column[item.dataset.note] + 'vh');
-//     } else {
-//         new_el.style.left = left_line_start + 'vh';
-//     }
-
-//     item.append(new_el);
-// }
-
-let generate_note_frame = function (arr_notes) {
-    let left_line_start = 38;
-    for (let i = 0; i < arr_notes.length; i++) {
-        all_line.forEach(function (elem) {
-            console.log(elem.dataset.note == arr_notes[i]);
-            if (elem.dataset.note == arr_notes[i]) {
-                let new_el = new_note_func();
-                new_el.style.left = left_line_start + 'vh';
-                left_line_start += 8;
-                elem.append(new_el);
-            }
-        })
-    }
+let obj_matrix = {
+    0: 'C1 D1 E1',
+    1: 'F1 G1 A1 B1',
+    2: 'C2 D2 E2',
+    3: 'F2 G2 A2 B2',
+    4: 'C3 D3 E3',
+    5: 'F3 G3 A3 B3',
+    6: 'C4 D4 E4',
+    7: 'F4 G4 A4 B4'
 }
 
-generate_note_frame(['C1', 'D1', 'E1'])
-generate_note_frame(['F1', 'G1', 'A1', 'B1'])
-generate_note_frame(['C2', 'D2', 'E2'])
+let generate_note_frame = function (obj_of_notes) {
+    for (const [key, value] of Object.entries(obj_of_notes)) {
+        // console.log(`${key}: ${value}`);
+        let left_line_start = 38;
+        let new_arr_of_string = value.split(' ');
+        // console.log(new_arr_of_string);
+        for (let i = 0; i < new_arr_of_string.length; i++) {
+            all_line.forEach(function (elem) {
+                // console.log(elem.dataset.note == new_arr_of_string[i]);
+                if (elem.dataset.note == new_arr_of_string[i]) {
+                    let new_el = new_note_func();
+                    new_el.dataset.note = elem.dataset.note;
+                    new_el.style.left = left_line_start + 'vh';
+                    left_line_start += 12;
+                    elem.append(new_el);
+                }
+            })
+            if (new_arr_of_string[i] === '0') {
+                left_line_start += 12;
+            }
+        }
+    }
+}
+generate_note_frame(obj_matrix);
+
+let clear_matrix = function () {
+    for (let item of all_line) {
+        item.innerHTML = '';
+    }
+}
+// clear_matrix();
+
+let value_of_add_ledon_class = '';
+let box_123 = 'C1 D1 E1 C2 D2 E2 C3 D3 E3 C4 D4 E4'
+let box_1234 = 'F1 G1 A1 B1 F2 G2 A2 B2 F3 G3 A3 B3 F4 G4 A4 B4'
+let add_ledon_class = function () {
+    document.querySelector('.container_with_keyboard').classList.remove('dspl_none');
+    if (value_of_add_ledon_class == '') {
+        for (let item of all_keyb_elem) {
+            item.classList.remove('led_on');
+            item.classList.remove('led_on_orange');
+            switch (item.dataset.note) {
+                case 'G1':
+                case 'B1':
+                case 'D2':
+                case 'F2':
+                case 'A2':
+
+                case 'E3':
+                case 'G3':
+                case 'B3':
+                case 'D4':
+                case 'F4':
+                    item.classList.add('led_on');
+                    break;
+            }
+        }
+        value_of_add_ledon_class = '10_line';
+    } else if (value_of_add_ledon_class == '10_line') {
+        for (let item of all_keyb_elem) {
+            item.classList.remove('led_on');
+            item.classList.remove('led_on_orange');
+            if ((box_123.indexOf(item.dataset.note)) >= 0) {
+                console.log('true');
+                item.classList.add('led_on_orange');
+            } else if ((box_1234.indexOf(item.dataset.note)) >= 0) {
+                item.classList.add('led_on');
+            }
+        }
+        value_of_add_ledon_class = '3+4';
+    } else if (value_of_add_ledon_class == '3+4') {
+        document.querySelector('.container_with_keyboard').classList.toggle('dspl_none');
+        value_of_add_ledon_class = '';
+    }
+}
+add_ledon_class();
+
+let add_label_of_note = function () {
+    let all_note = document.querySelectorAll('.note');
+    for (let item of all_note) {
+        item.innerHTML = item.dataset.note;
+    }
+}
