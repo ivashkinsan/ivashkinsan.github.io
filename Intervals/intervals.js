5fb06bf860b99ec5efa461a937aab31e2bdd7ab8
ivashkinsan <64753179+ivashkinsan@users.noreply.github.com>
2023-10-17 15:12:30 +0300

    vvv

--- Intervals/intervals.js
+++ Intervals/intervals.js
@@ -1,10 +1,14 @@
 let big_container = document.querySelector('.big_container');
 let all_keyb;
-let two_keys_for_int;
+let all_circle;
 let all_marker_bg_color;
 
-let all_keyb_elem_for_opacity;
+let checkbox_checked;
+// let marker_bg_color;
+let value_opacity_all_keyb = 0;
+
 let new_stage_vertiical_line;
+let value_opacity_vertical_line = 0;
 
 let stage_arr = '1 b2 2 b3 3 4 b5 5 b6 6 b7 7 1 b2 2 b3 3 4 b5 5 b6 6 b7 7 1 b2 2 b3 3 4 b5 5 b6 6 b7 7 1';
 let numb_string = '1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37';
@@ -99,76 +103,105 @@ let create_sphere = function (pattern) {
         }
         // arr_for_debugging.push(arr_row_for_debugging)
         big_container.append(new_div_column);
-
     }
-
-
 }
 
-
-
-
 // create_sphere(big_pattern_arr);
-
+let click_elem;
+let two_keys_for_int;
+let dump = [];
+let clickElemDatasetDump = [];
 
 big_container.addEventListener('click', (event) => {
-    let click_elem;
-    // console.log(event.target);
-    if (event.target.classList.contains('circle')) {
-        event.target.classList.toggle('active_box');
-        event.target.parentNode.classList.toggle('active_box');
+    // проверка клика на нужном элементе
+    if (event.target.classList.contains('circle')) { // проверка класса
+        event.target.classList.toggle('active_box'); // подсветка окружности
+        event.target.parentNode.classList.toggle('active_box'); // добавление класса родительскому див элементу
         click_elem = event.target;
+        // если клик произошел на Р или piano_keys элементе
     } else if (event.target.classList.contains('label_p') || event.target.classList.contains('piano_keys')) {
         event.target.parentNode.classList.toggle('active_box');
-        // event.target.parentNode.parentNode.classList.toggle('active_box');
         click_elem = event.target.parentNode;
-    }
-    if (click_elem.dataset.number && click_elem.dataset.summ) {
-        two_keys_for_int = Number(click_elem.dataset.number) + Number(click_elem.dataset.summ);
     }
     if (event.target.classList.contains('piano_keys')) {
         console.log('piano_keys')
         two_keys_for_int = '';
     }
-
-
-
-
-    let dump = [];
-    let clickElemDatasetDump = [];
     if (click_elem.classList.contains('active_box')) {
-        for (let i = 0; i < all_keyb.length; i++) {
-            if (all_keyb[i].dataset.number == click_elem.dataset.number || all_keyb[i].dataset.number == two_keys_for_int) {
-                let new_clone = all_keyb[i].cloneNode();
-                dump = Math.random();
-                if (!click_elem.dataset.dump) {
-                    click_elem.dataset.dump = dump;
-                } else {
-                    click_elem.dataset.dump = click_elem.dataset.dump + ',' + dump;
-                };
-                new_clone.dataset.dump = dump;
-                new_clone.classList.add('marker_bg_color');
-                all_keyb[i].parentNode.append(new_clone);
-            }
-        }
-
+        // функция клонирования-подсветки элемента и записи в дамп
+        add_marker_bg_color(click_elem, two_keys_for_int);
     } else {
-        clickElemDatasetDump = click_elem.dataset.dump.split(',');
-        // console.log(clickElemDatasetDump);
-        for (let i = 0; i < all_marker_bg_color.length; i++) {
-            if (clickElemDatasetDump.includes(all_marker_bg_color[i].dataset.dump)) {
-                all_marker_bg_color[i].remove();
-            }
-        }
-        clickElemDatasetDump = [];
+        all_marker_bg_color = document.querySelectorAll('.marker_bg_color');
+        remove_marker_bg_color(click_elem);
     }
-    all_marker_bg_color = document.querySelectorAll('.marker_bg_color');
 })
 
+// функция клонирования-подсветки элемента и записи в дамп
+let dragging = false;
+let startX;
+let startY;
+let add_marker_bg_color = function (elem, two_elem) {
+    dump = [];
+    clickElemDatasetDump = [];
+    if (elem.dataset.number && elem.dataset.summ) {
+        two_elem = Number(elem.dataset.number) + Number(elem.dataset.summ);
+    }
+    for (let i = 0; i < all_keyb.length; i++) {
+        if (all_keyb[i].dataset.number == elem.dataset.number || all_keyb[i].dataset.number == two_elem) {
+            let new_clone = all_keyb[i].cloneNode();
+            dump = Math.random();
+            if (!elem.dataset.dump) {
+                elem.dataset.dump = dump;
+            } else {
+                elem.dataset.dump = elem.dataset.dump + ',' + dump;
+            };
+            new_clone.dataset.dump = dump;
+
+
+            // new_clone.addEventListener('mousedown', (e) => {
+            //     dragging = true;
+            //     // Получаем стиль элемента:
+            //     const style = window.getComputedStyle(new_clone);
+            //     // Считываем значение каждой переменной через getPropertyValue:
+            //     const translateX = parseInt(style.getPropertyValue('--x'))
+            //     const translateY = parseInt(style.getPropertyValue('--y'))
+
+            //     // Дальше всё остаётся по-старому :–)
+            //     startX = e.pageX - translateX
+            //     startY = e.pageY - translateY
+            // })
+            // document.body.addEventListener('mousemove', (e) => {
+            //     if (!dragging) return
+
+            //     // Обратите внимание, насколько лаконичной стала запись.
+            //     // Мы всего лишь указываем, какое значение должна
+            //     // принять каждая из переменных:
+            //     new_clone.style.setProperty("--x", `${e.pageX - startX}px`)
+            //     new_clone.style.setProperty("--y", `${e.pageY - startY}px`)
+            // })
+
+            new_clone.draggable = true;
+            new_clone.classList.add('marker_bg_color');
+            all_keyb[i].parentNode.append(new_clone);
+        }
+    }
+    all_marker_bg_color = document.querySelectorAll('.marker_bg_color');
+}
 
 
 
+// функция удаления подсветки элемента и записей в дампе
+let remove_marker_bg_color = function (elem) {
+    clickElemDatasetDump = elem.dataset.dump.split(',');
+    for (let i = 0; i < all_marker_bg_color.length; i++) {
+        if (clickElemDatasetDump.includes(all_marker_bg_color[i].dataset.dump)) {
+            all_marker_bg_color[i].remove();
+        }
+    }
+    clickElemDatasetDump = [];
+};
 
+// колонка селект
 let sphere_select = document.querySelector('.sphere_select');
 sphere_select.addEventListener('change', (e) => {
     console.log(e.target.value);
@@ -224,32 +257,81 @@ sphere_select.addEventListener('change', (e) => {
             create_sphere(string_to_arr(B7));
             break;
     }
-    new_numb_arr();
-    all_keyb_elem_for_opacity = document.querySelectorAll('.piano_keys');
-    new_stage_vertiical_line = document.querySelectorAll('.new_stage_vertiical_line');
-    all_keyb = document.querySelectorAll('.piano_keys');
+    function_on_start();
+    if (checkbox_checked == true) {
+        add_active_box_for_all_elem();
+    }
+
 })
 
+
+let function_on_start = function () {
+    new_numb_arr();
+    all_marker_bg_color = document.querySelectorAll('.marker_bg_color');
+    new_stage_vertiical_line = document.querySelectorAll('.new_stage_vertiical_line');
+    all_keyb = document.querySelectorAll('.piano_keys');
+    all_circle = document.querySelectorAll('.circle');
+
+    add_opacity(new_stage_vertiical_line, value_opacity_vertical_line);
+    add_opacity(all_keyb, value_opacity_all_keyb);
+    add_opacity(all_marker_bg_color, value_opacity_all_keyb);
+}
 // create_sphere(string_to_arr(symmetry_3));
 
 
-// прозрачность клавиатуры
+// настройка прозрачности клавиатуры
 let opacity_input = document.querySelector('.input_opacity_piano_keys');
 opacity_input.addEventListener('input', (e) => {
-    for (item of all_keyb_elem_for_opacity) {
+    value_opacity_all_keyb = e.target.value / 100;
+    add_opacity(all_keyb, value_opacity_all_keyb);
+    add_opacity(all_marker_bg_color, value_opacity_all_keyb);
+
+})
+
+// настройка прозрачности маркеров ступеней
+let input_opacity_vertical_line = document.querySelector('.input_opacity_vertical_line');
+input_opacity_vertical_line.addEventListener('input', (e) => {
+    value_opacity_vertical_line = e.target.value / 100;
+    for (item of new_stage_vertiical_line) {
         item.style.opacity = e.target.value / 100;
-        item.style.zIndex = 0;
-        if (e.target.value == '100') {
-            item.style.zIndex = 1000;
-        }
     }
 })
 
-// прозрачность маркеров ступеней
-let input_opacity_vertical_line = document.querySelector('.input_opacity_vertical_line');
-input_opacity_vertical_line.addEventListener('input', (e) => {
-    for (item of new_stage_vertiical_line) {
-        item.style.opacity = e.target.value / 100;
-
+// универсальная функция прозрачности
+let add_opacity = function (elems, value) {
+    if (elems) {
+        for (let item of elems) {
+            item.style.opacity = value;
+            item.style.zIndex = 0;
+            if (value == 1) {
+                item.style.zIndex = 100;
+            }
+        }
     }
+}
+
+// активация всех окружностей
+let add_active_box_for_all_elem = function () {
+    for (let item of all_circle) {
+        item.classList.add('active_box');
+        add_marker_bg_color(item);
+    }
+}
+// активация всех окружностей
+let remove_active_box_for_all_elem = function () {
+    for (let item of all_circle) {
+        item.classList.remove('active_box');
+        remove_marker_bg_color(item);
+    }
+}
+// подключение всех маркеров
+let checkbox_for_marker = document.querySelector('.checkbox_for_add_all_bg_color');
+checkbox_for_marker.addEventListener('input', (e) => {
+    checkbox_checked = e.target.checked;
+    if (checkbox_checked == true) {
+        add_active_box_for_all_elem();
+    } else {
+        remove_active_box_for_all_elem();
+    }
+
 })
