4d808e6765d081a759e5665a72d205db30ffd2ef
ivashkinsan <64753179+ivashkinsan@users.noreply.github.com>
2024-03-02 09:22:20 +0300

    Update webAudioApi.js

--- keyboard_v2/webAudioApi.js
+++ keyboard_v2/webAudioApi.js
@@ -1,32 +1,55 @@
-let ctx = new(window.AudioContext || window.webkitAudioContext)();
-const frequencyRange = document.querySelector('.range_slide');
+// let ctx = new(window.AudioContext || window.webkitAudioContext)();
+// const frequencyRange = document.querySelector('.range_slide');
 // let ctx = new window.AudioContext();
 // console.log(ctx);
 
-let osc = ctx.createOscillator();
-console.log(osc);
+// let osc = ctx.createOscillator();
+// console.log(osc);
 
 
 // let gain = ctx.createGain();
 // console.log(gain);
 
-osc.frequency.value = 880;
+// osc.frequency.value = 880;
 // osc.type = 'sine';
 // osc.type = 'square';
 // osc.type = 'triangle';
 // osc.type = 'sawtooth';
-osc.connect(ctx.destination);
+// osc.connect(ctx.destination);
 
 
-let clear_2 = document.querySelector('.clear_2');
-clear_2.addEventListener('click', function () {
-    osc.start();
-    // setTimeout(() => {
-    //     osc.stop()
-    // }, 2000)
-});
+// let clear_2 = document.querySelector('.clear_2');
+// clear_2.addEventListener('click', function () {
+//     osc.start();
 
-frequencyRange.addEventListener('input', function (event) {
-    console.log(event.target.value);
-    osc.frequency.value = event.target.value;
-})
+// });
+
+// frequencyRange.addEventListener('input', function (event) {
+//     console.log(event.target.value);
+//     osc.frequency.value = event.target.value;
+// })
+
+const audioContext = new(window.AudioContext || window.webkitAudioContext)();
+
+console.log(AudioContext.sampleRate);
+
+const buffer = audioContext.createBuffer(
+    1,
+    audioContext.sampleRate * 1,
+    audioContext.sampleRate
+);
+
+const channelData = buffer.getChannelData(0);
+console.log(channelData, length);
+for (let i = 0; i < buffer.length; i++) {
+    channelData[i] = Math.random() * 2 - 1;
+}
+
+const whiteNoiseSource = audioContext.createBufferSource();
+whiteNoiseSource.buffer = buffer;
+
+const primaryGainControl = audioContext.createGain();
+primaryGainControl.gain.setValueAtTime(0.05, 0);
+
+whiteNoiseSource.connect(primaryGainControl);
+primaryGainControl.connect(audioContext.destination);
