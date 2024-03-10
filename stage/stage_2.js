659d2a8dc32116b7a36fc3d51b1083781254e3d0
ivashkinsan <64753179+ivashkinsan@users.noreply.github.com>
2024-03-11 01:03:54 +0300

    commit

---
+++ stage/stage_2.js
@@ -0,0 +1,43 @@
+let contains = document.querySelector('.contains');
+
+// note_opt_arr = note_str.split(' ');
+function shuffle(array) {
+    array.sort(() => Math.random() - 0.5);
+    return array;
+}
+
+function create_answer(){
+    let note_str_arr = ['C', 'C#/Db', 'D', 'D#/Eb', 'E', 'F', 'F#/Gb', 'G', 'G#/Ab', 'A', 'A#/Bb', 'B'];
+    let stage_arr = ['1', 'b2', '2', 'b3', '3', '4', 'b5', '5', 'b6', '6', 'b7', '7'];
+    let root_ton = ['C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'G', 'Ab', 'A', 'Bb', 'B'];
+
+    const new_obj = new Object();
+    const new_obj_child  = new Object();
+    for(let i = 0; i < root_ton.length; i++){
+        let del_el;
+        for (let j = 0; j < root_ton.length; j++) {
+            new_obj_child[stage_arr[j]] = note_str_arr[j];
+            // console.log( stage_arr[j] + ' = ' + note_str_arr[j]);
+             }
+        new_obj[root_ton[i]] = Object.assign({},new_obj_child);
+        del_el = note_str_arr.shift();
+        // console.log(note_str_arr);
+        note_str_arr = [ ...note_str_arr, del_el];
+    }
+// console.log(new_obj);
+    return new_obj;
+}
+create_answer();
+
+let answers = create_answer();
+console.log(create_answer());
+
+function create_select(answers){
+
+    for(let item in answers){
+        let options_container = document.createElement('div');
+        console.log(item);
+        document.querySelector('body').append(item);
+    }
+}
+create_select(answers);
