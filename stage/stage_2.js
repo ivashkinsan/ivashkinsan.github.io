7bcc64fe8a3f3c9712722613a5ed15a13401ce36
ivashkinsan <64753179+ivashkinsan@users.noreply.github.com>
2024-03-13 14:57:49 +0300

    123

--- stage/stage_2.js
+++ stage/stage_2.js
@@ -6,38 +6,36 @@ function shuffle(array) {
     return array;
 }
 
-function create_answer(){
+function create_answer() {
     let note_str_arr = ['C', 'C#/Db', 'D', 'D#/Eb', 'E', 'F', 'F#/Gb', 'G', 'G#/Ab', 'A', 'A#/Bb', 'B'];
     let stage_arr = ['1', 'b2', '2', 'b3', '3', '4', 'b5', '5', 'b6', '6', 'b7', '7'];
     let root_ton = ['C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'G', 'Ab', 'A', 'Bb', 'B'];
 
     const new_obj = new Object();
-    const new_obj_child  = new Object();
-    for(let i = 0; i < root_ton.length; i++){
+    const new_obj_child = new Object();
+    for (let i = 0; i < root_ton.length; i++) {
         let del_el;
         for (let j = 0; j < root_ton.length; j++) {
             new_obj_child[stage_arr[j]] = note_str_arr[j];
-            // console.log( stage_arr[j] + ' = ' + note_str_arr[j]);
-             }
-        new_obj[root_ton[i]] = Object.assign({},new_obj_child);
+        }
+        new_obj[root_ton[i]] = Object.assign({}, new_obj_child);
         del_el = note_str_arr.shift();
-        // console.log(note_str_arr);
-        note_str_arr = [ ...note_str_arr, del_el];
+        note_str_arr = [...note_str_arr, del_el];
     }
-// console.log(new_obj);
     return new_obj;
 }
-create_answer();
 
-let answers = create_answer();
-console.log(create_answer());
-
-function create_select(answers){
-
-    for(let item in answers){
+function create_select_box(answers) {
+    console.log(answers);
+    // 12 options_containers
+    // answers.forEach(element => {
+    //     console.log(element);
+    // });
+    for (let item in answers) {
         let options_container = document.createElement('div');
-        console.log(item);
-        document.querySelector('body').append(item);
+        console.log(answers[item]);
+        document.querySelector('.app').append(item);
     }
 }
-create_select(answers);
+
+create_select_box(create_answer());
