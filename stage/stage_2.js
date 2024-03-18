ab691589dee190797464096c9054d8b70ac18e31
ivashkinsan <64753179+ivashkinsan@users.noreply.github.com>
2024-03-18 09:02:02 +0300

    Update stage_2.js

--- stage/stage_2.js
+++ stage/stage_2.js
@@ -25,38 +25,59 @@ function create_answer() {
     return new_obj;
 }
 
-function create_select_box(answers) {
+function create_select_contains(answers) {
     console.log(answers);
     let obj_key_to_array = Object.keys(answers);
     let obj_values_to_array = Object.values(answers);
-    console.log(obj_key_to_array);
-    console.log(obj_values_to_array);
+    // console.log(obj_key_to_array);
+    // console.log(obj_values_to_array);
     for (let item of obj_key_to_array) {
-        console.log(answers[item]);
+        // console.log(answers[item]);
         let row_contain = document.createElement('div');
-        let label_root_ton = document.createElement('div');
+        let label_root_ton = document.createElement('h2');
         label_root_ton.textContent = item;
 
-
         row_contain.append(label_root_ton);
+        row_contain.append(create_row_selects(obj_values_to_array));
+        
         document.querySelector('.app').append(row_contain);
     }
-    // answers.forEach(function (value, index, array) {
-    // let options_container = document.createElement('select');
-    // 
-    // label_options_container.textContent = Object.keys(value);
-    // console.log(Object.keys(answers));
-    // console.log(answers[item]);
-    // for (let item_2 in answers[item]) {
-    //     let new_options = document.createElement('option');
-    //     new_options.value = answers[item][item_2];
-    //     new_options.textContent = answers[item].value;
 
-    //     options_container.append(new_options)
-    // }
-    // document.querySelector('.app').append(label_options_container);
-    // 
-    // })
 }
 
-create_select_box(create_answer());
+create_select_contains(create_answer());
+
+function create_one_select_elem(input){
+    let input_to_arr = Object.values(input);
+    console.log(input_to_arr);
+    // let i = 0;
+
+    let select_elem = document.createElement('select');
+    if(typeof input == 'object'){
+        for(elem in input){
+            // let label_for_select_elem = document.createElement('div');
+            // label_for_select_elem.textContent = input_to_arr[i];
+            // i++;
+
+            let select_elem_option = document.createElement('option');
+            select_elem_option.textContent = elem;
+            // select_elem.append(label_for_select_elem);
+            select_elem.append(select_elem_option);
+        }
+
+    }
+      return select_elem;
+}
+
+function create_row_selects(input){
+let one_row = document.createElement('div');
+
+for(let i = 0; i < input.length; i++){
+    let label_for_select_elem = document.createElement('div');
+    label_for_select_elem.textContent = input[i];
+    one_row.append(label_for_select_elem);
+    one_row.append(create_one_select_elem(input[i]));
+        
+}
+return one_row;
+}
