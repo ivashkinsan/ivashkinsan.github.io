f0d1621412f2b7e8424a4f48e429fb08afd90855
ivashkinsan <64753179+ivashkinsan@users.noreply.github.com>
2024-03-18 11:50:34 +0300

    123

--- stage/stage_2.js
+++ stage/stage_2.js
@@ -8,7 +8,7 @@ function shuffle(array) {
 
 function create_answer() {
     let note_str_arr = ['C', 'C#/Db', 'D', 'D#/Eb', 'E', 'F', 'F#/Gb', 'G', 'G#/Ab', 'A', 'A#/Bb', 'B'];
-    let stage_arr = ['1', 'b2', '2', 'b3', '3', '4', 'b5', '5', 'b6', '6', 'b7', '7'];
+    let stage_arr = ['1', 'b2', '2', 'b3', '3', '4', '#4/b5', '5', 'b6', '6', 'b7', '7'];
     let root_ton = ['C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'G', 'Ab', 'A', 'Bb', 'B'];
 
     const new_obj = new Object();
@@ -29,17 +29,14 @@ function create_select_contains(answers) {
     console.log(answers);
     let obj_key_to_array = Object.keys(answers);
     let obj_values_to_array = Object.values(answers);
-    // console.log(obj_key_to_array);
-    // console.log(obj_values_to_array);
     for (let item of obj_key_to_array) {
-        // console.log(answers[item]);
         let row_contain = document.createElement('div');
         let label_root_ton = document.createElement('h2');
         label_root_ton.textContent = item;
 
         row_contain.append(label_root_ton);
-        row_contain.append(create_row_selects(obj_values_to_array));
-        
+        row_contain.append(create_row_selects(obj_values_to_array, 0));
+
         document.querySelector('.app').append(row_contain);
     }
 
@@ -47,37 +44,52 @@ function create_select_contains(answers) {
 
 create_select_contains(create_answer());
 
-function create_one_select_elem(input){
-    let input_to_arr = Object.values(input);
-    console.log(input_to_arr);
-    // let i = 0;
 
+
+function create_one_select_elem(input, answer) {
     let select_elem = document.createElement('select');
-    if(typeof input == 'object'){
-        for(elem in input){
-            // let label_for_select_elem = document.createElement('div');
-            // label_for_select_elem.textContent = input_to_arr[i];
-            // i++;
+    let null_elem = document.createElement('option');
+
+    null_elem.textContent = '--';
+    select_elem.append(null_elem);
+    console.log(input);
+    if (typeof input == 'object') {
+        for (elem in input) {
 
             let select_elem_option = document.createElement('option');
             select_elem_option.textContent = elem;
-            // select_elem.append(label_for_select_elem);
+            select_elem_option.value = input[elem];
             select_elem.append(select_elem_option);
         }
 
     }
-      return select_elem;
+    select_elem.dataset.answer = answer;
+
+    return select_elem;
 }
 
-function create_row_selects(input){
-let one_row = document.createElement('div');
+function create_row_selects(input, indx) {
+    let one_row = document.createElement('div');
+    one_row.classList.add('one_row');
+    let input_to_arr = shuffle(Object.values(input[indx]));
 
-for(let i = 0; i < input.length; i++){
-    let label_for_select_elem = document.createElement('div');
-    label_for_select_elem.textContent = input[i];
-    one_row.append(label_for_select_elem);
-    one_row.append(create_one_select_elem(input[i]));
-        
-}
-return one_row;
+    for (let i = 0; i < input.length; i++) {
+        let one_box_for_row = document.createElement('div');
+        one_box_for_row.classList.add('one_box_for_row');
+
+        // console.log(input_to_arr[indx]);
+        // let i = 0;
+        let label_for_select_elem = document.createElement('div');
+        label_for_select_elem.classList.add('label_for_select_elem');
+        label_for_select_elem.textContent = input_to_arr[indx];
+
+        one_box_for_row.append(label_for_select_elem);
+        one_box_for_row.append(
+            create_one_select_elem(input[i], input_to_arr[indx])
+        );
+        one_row.append(one_box_for_row);
+        indx++;
+    }
+
+    return one_row;
 }
