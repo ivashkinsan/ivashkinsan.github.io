96d8c662700f1f583b85e44fc4d2495762d28fd6
ivashkinsan <64753179+ivashkinsan@users.noreply.github.com>
2024-04-08 09:12:34 +0300

    midi

--- midi/midi.js
+++ midi/midi.js
@@ -1,6 +1,15 @@
-function loadMidiFile() {
-    MidiPlayer.load('/midi/Elka.mid', function(data) {
-        console.log(data);
-    });
+// import MIDI from 'midi.js'
+
+// function loadMidiFile() {
+//     MidiPlayer.load('/midi/Elka.mid', function (data) {
+//         console.log(data);
+//     });
+// }
+// loadMidiFile();
+
+
+
+// http://prgssr.ru/development/vvedenie-v-web-midi.html
+if (navigator.requestMIDIAccess) {
+    console.log('Browser supports MIDI!');
 }
-loadMidiFile();
