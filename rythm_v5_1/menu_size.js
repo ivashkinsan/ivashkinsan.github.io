bf84abf28ffec006635ddc7c6ec6146422625051
ivashkinsan <64753179+ivashkinsan@users.noreply.github.com>
2024-05-30 08:26:26 +0300

    Update menu_size.js

--- rythm_v5_1/menu_size.js
+++ rythm_v5_1/menu_size.js
@@ -9,6 +9,11 @@ const sizeMenu = new Object({
     'variants': ['2:4', '3:4', '4:4', '5:4', '6:4', '7:4', '8:4', '6:8', '9:8', '12:8', 'sw','sw','sw'],
     'thumbler_text': ['\uF078','\uF065','3 \uF065'],
     'button': null,
+    'thumbler': {
+        'sixteenthNote_16': true,
+        'eighthNote_8': true,
+        'eighthNote_8_triple': false,
+    },
     'contain': null,
     'elementsArr': [],
     createMenu() {
@@ -126,3 +131,25 @@ sizeMenu.createButtonSizeMenu();
 
 let app = document.querySelector('.app');
 app.before(sizeMenu.button);
+console.log(sizeMenu)
+
+
+const proxyThumbler = new Proxy(sizeMenu.thumbler, {
+    set(target, key, value) {
+        target[key] = value;
+        if(target[value] == 'sixteenthNote_16' && value == false){
+            console.log('set target false');
+            console.log(`value = ${value}`)
+            console.log(`target[key] = ${target[key]}`)
+            console.log(target);
+        }
+        if(value == true){
+            console.log('set target false');
+            console.log(`value = ${value}`)
+            console.log(`target[key] = ${target[key]}`)
+            console.log(target);
+        }
+
+        return true;
+    }
+});
