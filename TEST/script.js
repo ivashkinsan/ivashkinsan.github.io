0ee59ef5a9cf5561a7bf32a5318db311d18bb7c9
ivashkinsan <64753179+ivashkinsan@users.noreply.github.com>
2024-06-03 13:01:46 +0300

    Update script.js

--- TEST/script.js
+++ TEST/script.js
@@ -79,10 +79,16 @@ const createKeyboard = function(inputContent){
     return keybContainer;
 }
 
-const createSelectStage = function(){
+const saveState = function(input){
+    localStorage.setItem(input.id, input.value);
+}
+
+const createSelectStage = function(input, value){
+    
     let optionValue = '-- 1 b2 2 b3 3 4 #4/b5 5 b6 6 b7 7';
     let optionArr = optionValue.split(' ');
     let select = document.createElement('select');
+    
     select.classList.add('select_stage');
     for(let i = 0; i < optionArr.length; i++){
         let option = document.createElement('option');
@@ -90,6 +96,14 @@ const createSelectStage = function(){
         option.textContent = optionArr[i];
         select.appendChild(option);
     }
+
+    if(window.localStorage[`createSelectStage_${input}`]){
+        console.log(`createSelectStage_${input}`);
+        select.classList.add('winner');
+        select.value = window.localStorage[`createSelectStage_${input}`];
+        select.disabled = true;
+    }
+
     select.addEventListener('change', (e)=>{
         // console.log(e.target.parentNode.dataset.value);
         if(e.target.parentNode.dataset.valueStage == e.target.value){
@@ -97,6 +111,10 @@ const createSelectStage = function(){
             e.target.disabled = true;
             // e.target.parentNode.classList.add('winner');
             addCount('true');
+            let storage = {};
+            storage.id = `createSelectStage_${input}`;
+            storage.value = value;
+            saveState(storage);
         } else {
             addCount();
         }
@@ -104,7 +122,7 @@ const createSelectStage = function(){
     return select;
 }
 
-const createSelectTetrachords = function(){
+const createSelectTetrachords = function(input,value){
     let optionValue = '-- maj min frig harm TTT';
     let optionArr = optionValue.split(' ');
     let select = document.createElement('select');
@@ -115,6 +133,14 @@ const createSelectTetrachords = function(){
         option.textContent = optionArr[i];
         select.appendChild(option);
     }
+
+    if(window.localStorage[`createSelectTetrachords_${input}`]){
+        console.log(`createSelectTetrachords_${input}`);
+        select.classList.add('winner');
+        select.value = window.localStorage[`createSelectTetrachords_${input}`];
+        select.disabled = true;
+    }
+
     select.addEventListener('change', (e)=>{
         // console.log(e.target.parentNode.dataset.value);
         if(e.target.parentNode.dataset.valueTetrachord == e.target.value){
@@ -122,6 +148,10 @@ const createSelectTetrachords = function(){
             e.target.disabled = true
             // e.target.parentNode.classList.add('winner');
             addCount('true');
+            let storage = {};
+            storage.id = `createSelectTetrachords_${input}`;
+            storage.value = value;
+            saveState(storage);
         } else {
             addCount();
         }
@@ -129,7 +159,7 @@ const createSelectTetrachords = function(){
     return select;
 }
 
-const createSelectChords = function(){
+const createSelectChords = function(input, value){
     let optionValue = '-- maj min dim aug';
     let optionArr = optionValue.split(' ');
     let select = document.createElement('select');
@@ -140,12 +170,24 @@ const createSelectChords = function(){
         option.textContent = optionArr[i];
         select.appendChild(option);
     }
+
+    if(window.localStorage[`createSelectChords_${input}`]){
+        console.log(`createSelectChords_${input}`);
+        select.classList.add('winner');
+        select.value = window.localStorage[`createSelectChords_${input}`];
+        select.disabled = true;
+    }
+
     select.addEventListener('change', (e)=>{
         // console.log(e.target.parentNode.dataset.value);
         if(e.target.parentNode.dataset.valueChord == e.target.value){
             e.target.classList.add('winner');
             e.target.disabled = true
             addCount('true');
+            let storage = {};
+            storage.id = `createSelectChords_${input}`;
+            storage.value = value;
+            saveState(storage);
         } else {
             addCount();
         }
@@ -153,7 +195,7 @@ const createSelectChords = function(){
     return select;
 }
 
-const createSeventhChords = function(){
+const createSeventhChords = function(input, value){
     let optionValue = '-- X^ X^#5 Xm^ X7 Xm7 Xm7b5 Xo';
     let optionArr = optionValue.split(' ');
     let select = document.createElement('select');
@@ -164,12 +206,24 @@ const createSeventhChords = function(){
         option.textContent = optionArr[i];
         select.appendChild(option);
     }
+
+    if(window.localStorage[`createSeventhChords_${input}`]){
+        console.log(`createSeventhChords_${input}`);
+        select.classList.add('winner');
+        select.value = window.localStorage[`createSeventhChords_${input}`];
+        select.disabled = true;
+    }
+
     select.addEventListener('change', (e)=>{
         // console.log(e.target.parentNode.dataset.value);
         if(e.target.parentNode.dataset.valueSeventhСhords == e.target.value){
             e.target.classList.add('winner');
             e.target.disabled = true
             addCount('true');
+            let storage = {};
+            storage.id = `createSeventhChords_${input}`;
+            storage.value = value;
+            saveState(storage);
         } else {
             addCount();
         }
@@ -184,7 +238,9 @@ const createJob = function(inputJob){
     let tetrachords = inputJob.tetrachords.split(' ');
     let chords = inputJob.chords.split(' ');
     let seventhСhords = inputJob.seventhСhords.split(' ');
-    for(let item of inputJob.content.split(' ')){
+    let arrForOf = inputJob.content.split(' ');
+    for(let i = 0; i < arrForOf.length; i++ ){
+        
         let rowElem = document.createElement('div');
         rowElem.classList.add('row_elem');
         rowElem.dataset.valueStage = stage.shift();
@@ -193,13 +249,13 @@ const createJob = function(inputJob){
         rowElem.dataset.valueSeventhСhords = seventhСhords.shift();
 
         let rowElemChild = document.createElement('p');
-        rowElemChild.textContent = item;
+        rowElemChild.textContent = arrForOf[i];
 
         rowElem.appendChild(rowElemChild);
-        rowElem.append(createSelectStage());
-        rowElem.append(createSelectTetrachords());
-        rowElem.append(createSelectChords());
-        rowElem.append(createSeventhChords());
+        rowElem.append(createSelectStage(i, rowElem.dataset.valueStage));
+        rowElem.append(createSelectTetrachords(i, rowElem.dataset.valueTetrachord));
+        rowElem.append(createSelectChords(i, rowElem.dataset.valueChord));
+        rowElem.append(createSeventhChords(i, rowElem.dataset.valueSeventhСhords));
         newRow.appendChild(rowElem);
     }
 
@@ -209,3 +265,4 @@ const createJob = function(inputJob){
 
 
 createJob(job.row_1);
+// localStorage.clear();
